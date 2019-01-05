692dbedd5debbd24ee6453a5479af8d3104f0a57
mmitkevich <mim@exante.eu>
2019-01-05 03:13:30 +0300

    eosio's standalone: conan

---
+++ conanfile.py
@@ -0,0 +1,45 @@
+from conans import ConanFile, CMake, tools
+
+
+class fcConan(ConanFile):
+    name = "eos-fc"
+    version = "e5ad1ad"
+    license = "<Put the package license here>"
+    author = "<Put your name here> <And your email here>"
+    url = "<Package recipe repository url here, for issues about the package>"
+    description = "<Description of Utxx here>"
+    topics = ("<Put some tag here>", "<here>", "<and here>")
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+
+    def source(self):
+        self.run("git clone https://github.com/eosio/fc.git")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.definitions["TOOLCHAIN"] = "clang" 
+        cmake.definitions["CMAKE_MODULE_PATH"] = "CMakeModules"
+	cmake.configure(source_folder="fc")
+        self.run('git submodule init && git submodule update')
+        cmake.build()
+
+        #Explicit way:
+        
+        #self.run('cmake %s %s'
+        #          % (self.source_folder, cmake.command_line))
+        #self.run("cmake --build . %s" % cmake.build_config)
+
+    def package(self):
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["fc"]
+
+    def requirements(self):
+        self.requires("boost/1.67.0@conan/stable")
