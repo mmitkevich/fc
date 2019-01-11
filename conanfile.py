3c5ce522363a02e925f5078cb2a508ae1fb6ed23
mmitkevich <mim@exante.eu>
2019-01-11 16:33:09 +0300

    fixed conan

--- conanfile.py
+++ conanfile.py
@@ -36,7 +36,7 @@ class fcConan(ConanFile):
         self.copy("*.so", dst="lib", keep_path=False)
         self.copy("*.dylib", dst="lib", keep_path=False)
         self.copy("*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*", dst="include", src="fc/include/*")
+        self.copy(pattern="*", dst="include", src="fc/include")
     def package_info(self):
         self.cpp_info.libs = ["fc"]
 
